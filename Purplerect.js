6328cae092d0c09dc4685936da46a89a90407a4c
Gautam-savage <72249239+Gautam-savage@users.noreply.github.com>
2020-10-31 18:54:04 +0530

    Add files via upload

---
+++ Purplerect.js
@@ -0,0 +1,36 @@
+class Purplerect {
+    constructor(x,y,width,height,angle){
+      var options = {
+        'restitution':0.8,
+        'friction':0.8,
+        'density':1.0
+    }
+    this.visibility=255;
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    World.add(world, this.body);
+      
+      
+      
+      //super(x,y,20,height,angle);
+      this.image = loadImage("purplerect.png");
+      Matter.Body.setAngle(this.body, angle);
+    }
+    display(){
+      if(this.body.speed<3){
+      var angle = this.body.angle;
+      push();
+      translate(this.body.position.x, this.body.position.y);
+      rotate(angle);
+      imageMode(CENTER);
+      image(this.image, 0, 0, this.width, this.height);
+      pop();
+      }else{
+        World.remove(world,this.body);
+        push();
+        this.visibility=this.visibility-5;
+        pop();
+      }
+    }
+  }
